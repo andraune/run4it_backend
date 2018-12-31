b75a190284f92d70908fef496c39e836f163096b
Andreas MacMini1 <andreas.aune@gmail.com>
2018-12-31 15:45:29 +0100

    Postman collections fixed, adjusting startup.

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,12 @@
-from run4it_backend import app
+"""Application entry point"""
+from run4it.app.app import create_app
+from run4it.app.config import get_environment_config
+from run4it.api.api_v1 import create_api
+
+
+app_config = get_environment_config()
+app = create_app(app_config, __name__)
+api = create_api(app)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0')
