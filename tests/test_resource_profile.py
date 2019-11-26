1fc6100149dd946594a9979c82949e57b65d9a8e
Andreas Aune <andreas.aune@norbit.com>
2019-11-26 15:15:04 +0100

    Fixed broken test

--- tests/test_resource_profile.py
+++ tests/test_resource_profile.py
@@ -7,6 +7,6 @@ from .helpers import get_response_json
 class TestProfileResource:
 
     def test_content_type_is_json(self, api, client):
-        url = api.url_for(Profile)
+        url = api.url_for(Profile, username="profileUsername")
         response = client.get(url)
         assert(response.headers["Content-Type"] == 'application/json')
