a2b1ec45cb26a38fa0bcd46838defc488d858145
Andreas Aune <andreas.aune@norbit.com>
2019-11-28 14:06:11 +0100

    Fixed inconsistent use of 'birth_date'

--- run4it/api/profile/schema.py
+++ run4it/api/profile/schema.py
@@ -4,7 +4,7 @@ from marshmallow import Schema, validate, fields
 
 class ProfileSchema(Schema):
     height = fields.Integer(validate=[validate.Range(0, 280)])
-    birth_date = fields.Date(validate=[validate.Range(dt.date(1900, 1, 1), dt.date.today())])
+    birthDate = fields.Date(attribute='birth_date', validate=[validate.Range(dt.date(1900, 1, 1), dt.date.today())])
     createdAt = fields.DateTime(attribute='created_at', dump_only=True)
     updatedAt = fields.DateTime(attribute='updated_at', dump_only=True)
     username = fields.Str(dump_only=True, required=True)
