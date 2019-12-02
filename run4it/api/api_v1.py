0dd3b33af1197e8dcf00d858ea066035ac69d833
Andreas Aune <andreas.aune@norbit.com>
2019-12-02 15:11:55 +0100

    Added weight table tests for newman.

--- run4it/api/api_v1.py
+++ run4it/api/api_v1.py
@@ -14,7 +14,7 @@ API_VERSION_STR = 'v{0}'.format(API_VERSION)
 
 class ApiVersion(Resource):
 	def get(self):
-		return { 'version': API_VERSION, 'env': current_app.config['ENV'], 'todo': 'Logout, LogoutRefresh, NewmanTests' }
+		return { 'version': API_VERSION, 'env': current_app.config['ENV'], 'todo': 'Logout, LogoutRefresh' }
 
 
 def create_api(app):
