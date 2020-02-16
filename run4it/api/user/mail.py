3809856ac17fc77b7c7d80df3b8b688ca9f66400
Andreas MacMini1 <andreas.aune@gmail.com>
2020-02-16 19:30:05 +0100

    Changed mail text.

--- run4it/api/user/mail.py
+++ run4it/api/user/mail.py
@@ -7,9 +7,8 @@ def mail_send_confirmation_code(username, email, code):
 	msg = Message('Run4IT Email Confirmation', recipients=[email])
 	msg.body = "Hi, {0}!\r\n\r\n".format(username)
 	msg.body += "Your confirmation code is: {0}\r\n\r\n".format(code)
-	msg.body += "Click the link below to complete your account registration. "
-	msg.body += "If the link does not work, you might copy the URL and enter it manually in your browser.\r\n\r\n"
-	msg.body += "Activation link: https://run4it.jonnytech.net/confirmation?username={0}&code={1}\r\n\r\n".format(username, code)
+	msg.body += "Copy the code and paste it into the 'Confirmation code' field in the confirmation page to complete the registration process.\r\n\r\n"
+	msg.body += "Or you can use the following link: https://run4it.jonnytech.net/confirmation?username={0}&code={1}\r\n\r\n".format(username, code)
 	msg.body += "If you haven't tried to register at our site please ignore this email.\r\n\r\nRegards, Jonny//Run4IT-team\r\n"
 
 	with mail.record_messages() as outbox:
