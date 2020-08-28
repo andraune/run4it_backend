8ec1dd8d210e192aef7d69d85e4b97bdd78f9b25
andreas.aune <andreas.aune@greenfox.no>
2020-08-28 11:15:43 +0200

    Simple logging framework

--- run4it_backend.py
+++ run4it_backend.py
@@ -1,12 +1,13 @@
 """Application entry point"""
+import logging, logging.config, yaml
 from run4it.app.app import create_app
 from run4it.app.config import get_environment_config
 from run4it.api.api_v1 import create_api
 
-
+logging.config.dictConfig(yaml.safe_load(open('logging.conf')))
 app_config = get_environment_config()
 app = create_app(app_config, __name__)
 api = create_api(app)
 
-if __name__ == "__main__":
+if __name__ == "__main__":   
     app.run(host='0.0.0.0')
